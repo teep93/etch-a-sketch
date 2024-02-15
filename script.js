658f772ac2cdf66d28320208502f285207dfa682
teep93 <roz@DESKTOP-FI7C3B1>
2024-02-15 23:02:31 +0800

    add row, cell, column functions

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-function addSquareDiv() {
-    const squareDiv = document.createElement("div")
-    squareDiv.classList.add("square");
-    let size = "100px";
-    squareDiv.style.width = size;
-    squareDiv.style.height = size;
-    document.body.appendChild(squareDiv)
-    
+function addRow() {
+  
+  let rowDiv = document.createElement("div");
+  rowDiv.classList.add("row");
+  addCell(rowDiv);
+  document.querySelector('.container').appendChild(rowDiv);
+  
 }
 
-for (let i = 0; i <= 16; i++) {
-    addSquareDiv();
+function addCell(parent) {
+  let cell = document.createElement("div");
+  cell.classList.add("cell");
+  let size = "100px";
+  cell.style.width = size;
+  cell.style.height = size;
+  parent.appendChild(cell);
 }
 
 
+function addColumn() {
+  let columnDiv = document.createElement("div");
+  columnDiv.classList.add("column");
+  addCell(columnDiv);
+  document.querySelector('.container').appendChild(columnDiv);
+}
 
+let gridSize = 2;
 
+function createGrid() {
+  addRow();
+  addColumn();
+  // addColumn();
+}
+  
 
-
+createGrid();
 
