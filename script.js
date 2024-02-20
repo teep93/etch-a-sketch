5d94d2cfec672b023d92c725af5731096bb1e903
teep93 <roz@DESKTOP-FI7C3B1>
2024-02-20 21:06:13 +0800

    add creategrid function

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let gridContainer = document.querySelector(".container");
-let totalNoSquares = 256;
+const row= 60;
+const column = 60;
+const height = 500;
+const width = 500;
 
+let outerContainer = document.querySelector('.container');
+outerContainer.style.width = `${width}px`;
+outerContainer.style.height = `${height}px`;
+console.log(height / row)
 
-
-function generateGrid() {
-  for (let i = 1; i <= totalNoSquares; i++) {
-    gridSquare = document.createElement("div");
-    let squareStyle = document.querySelector("div");
-    squareStyle.setAttribute('style', 'display: flex;', 'flex-wrap: wrap');
-    gridContainer.appendChild(gridSquare);
-  }
-}  
-
-generateGrid();
+function createGrid() {
+  for (let i = 1; i <= (row * column); i++) {
+    let innerContainer = document.createElement('div');
+    innerContainer.style.height = `${height / row}px`;
+    innerContainer.style.width = `${width / column}px`;
+    innerContainer.style.flex = 'auto';
+    outerContainer.appendChild(innerContainer);
+  };
+}
