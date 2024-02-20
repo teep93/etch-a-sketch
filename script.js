519f1d6cc19b91f2fc0f895c040a667031210e33
teep93 <roz@DESKTOP-FI7C3B1>
2024-02-20 23:52:54 +0800

    add hover effect

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-const row= 60;
-const column = 60;
+const row= 10;
+const column = 10;
 const height = 500;
 const width = 500;
+
 
 let outerContainer = document.querySelector('.container');
 outerContainer.style.width = `${width}px`;
 outerContainer.style.height = `${height}px`;
 console.log(height / row)
+let square = document.querySelectorAll('.squareDiv');
 
-function createGrid() {
-  for (let i = 1; i <= (row * column); i++) {
-    let innerContainer = document.createElement('div');
-    innerContainer.style.height = `${height / row}px`;
-    innerContainer.style.width = `${width / column}px`;
-    innerContainer.style.flex = 'auto';
-    outerContainer.appendChild(innerContainer);
+for (let i = 1; i <= (row * column); i++) {
+  let innerContainer = document.createElement('div');
+  innerContainer.classList.add('squareDiv');
+  innerContainer.style.height = `${height / row}px`;
+  innerContainer.style.width = `${width / column}px`;
+  innerContainer.style.flex = 'auto';
+  innerContainer.classList.add("hover");
+  outerContainer.appendChild(innerContainer);
+    
+    
   };
-}
+
+
+
+const hoverColor = document.querySelectorAll(".hover");
+hoverColor.forEach((square) => {
+  square.addEventListener("mouseenter", () => {
+    square.style.backgroundColor = 'black';
+  });
+// })  
+  square.addEventListener("mouseleave", () => {
+    square.style.backgroundColor = 'white';
+  });
+});
+
+
+
+
+
+
+
+
