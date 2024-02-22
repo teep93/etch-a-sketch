f582dcf2fb1480d19a6ebbd54aed661b91a428d3
teep93 <roz@DESKTOP-FI7C3B1>
2024-02-22 21:29:01 +0800

    add create new grid function

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
-const row= 10;
-const column = 10;
+let row = 4;
+let column = 4;
 const height = 500;
 const width = 500;
+let inputRow = '';
+let inputColumn = ''
 
 
-let outerContainer = document.querySelector('.container');
+
+let outerContainer = document.querySelector(".container");
 outerContainer.style.width = `${width}px`;
 outerContainer.style.height = `${height}px`;
-console.log(height / row)
-let square = document.querySelectorAll('.squareDiv');
+console.log(height / row);
+let squares = document.querySelectorAll(".squareDiv");
+let square = document.querySelector(".squareDiv")
 
-for (let i = 1; i <= (row * column); i++) {
-  let innerContainer = document.createElement('div');
-  innerContainer.classList.add('squareDiv');
-  innerContainer.style.height = `${height / row}px`;
-  innerContainer.style.width = `${width / column}px`;
-  innerContainer.style.flex = 'auto';
-  innerContainer.classList.add("hover");
-  outerContainer.appendChild(innerContainer);
-    
-    
-  };
+function createNewGrid(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+  templateGrid();
+}  
+  
 
 
 
-const hoverColor = document.querySelectorAll(".hover");
-hoverColor.forEach((square) => {
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = 'black';
+function templateGrid() {
+  for (let i = 1; i <= row * column; i++) {
+    let innerContainer = document.createElement("div");
+    innerContainer.classList.add("squareDiv");
+    innerContainer.style.height = `${height / row}px`;
+    innerContainer.style.width = `${width / column}px`;
+    innerContainer.style.flex = "auto";
+    innerContainer.classList.add("hover");
+    outerContainer.appendChild(innerContainer);
+  }
+  addHover();
+}
+
+templateGrid();
+
+function addHover() {
+  const hoverColor = document.querySelectorAll(".hover");
+  hoverColor.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "black";
+    });
   });
-// })  
-  square.addEventListener("mouseleave", () => {
-    square.style.backgroundColor = 'white';
-  });
+}
+  
+const newGrid = document.querySelector("#newGrid");
+newGrid.addEventListener("click", () => {
+  createNewGrid(outerContainer);
+
 });
-
-
-
-
-
-
-
 
